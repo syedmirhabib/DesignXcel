fef90629bc67244def52784af9feec715579e293
Syed Mir Habib <mirakik.habib@gmail.com>
2023-06-08 09:34:27 +0600

    update linting rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true},
+  env: { 
+    browser: true, 
+    es2020: true, 
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -11,5 +15,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "react/prop-types": "off"
   },
 }
