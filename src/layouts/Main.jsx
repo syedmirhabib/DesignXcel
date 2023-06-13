d07ece8ebf004c445b7313879d1d8db0363365da
Syed Mir Habib <mirakik.habib@gmail.com>
2023-06-13 14:20:49 +0600

    layout Main added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,18 +1,35 @@
-import { Outlet, useLocation } from "react-router-dom";
+// import { Outlet, useLocation } from "react-router-dom";
+// import NavBar from "../pages/Shared/NavBar/NavBar";
+// import Footer from "../pages/Shared/Footer/Footer";
+
+import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
-import NavBar from "../pages/Shared/NavBar/NavBar";
+
+
+// const Main = () => {
+//     const location = useLocation();
+    
+//     const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
+
+//     return (
+//         <div>
+//             { noHeaderFooter || <NavBar></NavBar>}
+//             <Outlet></Outlet>
+//             { noHeaderFooter || <Footer></Footer>}
+//         </div>
+//     );
+// };
+
+// export default Main;
+
+
 
 
 const Main = () => {
-    const location = useLocation();
-    
-    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
-
     return (
         <div>
-            { noHeaderFooter || <NavBar></NavBar>}
             <Outlet></Outlet>
-            { noHeaderFooter || <Footer></Footer>}
+            <Footer></Footer>
         </div>
     );
 };
