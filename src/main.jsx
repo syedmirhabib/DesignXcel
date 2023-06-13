bff0660102f3417d83d01cbc3ed639ec77019b44
Syed Mir Habib <mirakik.habib@gmail.com>
2023-06-13 14:17:06 +0600

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from "./routes/Routes";
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
