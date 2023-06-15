a0124877d1d85bc0722cc35bae749f30ae43f31d
Syed Mir Habib <mirakik.habib@gmail.com>
2023-06-16 00:55:47 +0600

    toaster and strictMood is removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from "./routes/Routes";
-
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import AuthProvider from "./providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <AuthProvider>
+    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </QueryClientProvider>
+  </AuthProvider>
 );
